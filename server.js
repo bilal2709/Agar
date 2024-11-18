1f5d38a18f95419a4a1803d31fccc8f8887b7ee3
bilal <bilal-rockstar@outlook.com>
2024-11-18 15:01:56 +0100

    add music + connection and disconnection for Players

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 const express = require('express');
-const app = express();
+const http = require('http');
+const { Server } = require('socket.io');
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'front')));
-const PORT = 3000;
+const app = express();
+const server = http.createServer(app);
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('Un joueur est connecté :', socket.id);
+
+
+  socket.on('message', (data) => {
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Joueur déconnecté :', socket.id);
+  });
 });
+
+app.use(express.static(path.join(__dirname, 'front')));
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Serveur en écoute sur le port ${PORT}`);
+});
+
